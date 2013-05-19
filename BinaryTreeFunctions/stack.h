eecc9812a08f5b561964cfa47e7841fddc485942
asudhindra <arunsudhindra@gmail.com>
2013-05-19 17:15:42 -0400

    Initial commit

---
+++ BinaryTreeFunctions/stack.h
@@ -0,0 +1,17 @@
+//
+//  stack.h
+//  BinaryTreeFunctions
+//
+//  Created by Arun Sudhindra on 5/19/13.
+//  Copyright (c) 2013 Arun Sudhindra. All rights reserved.
+//
+
+#ifndef BinaryTreeFunctions_stack_h
+#define BinaryTreeFunctions_stack_h
+
+void push( char );
+char pop();
+int empty();
+int full();
+
+#endif
